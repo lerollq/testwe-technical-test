548b93be4bc43df2bf451ce9a3691e7e691683c0
Quentin Le Rolland <qlerolland@gmail.com>
2022-12-09 21:56:27 +0100

    feat(api): initialize base api with rtk query

---
+++ src/api/base.api.ts
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_API_URL } from "../config/constants";
+
+const baseApi = createApi({
+	baseQuery: fetchBaseQuery({
+		baseUrl: BASE_API_URL,
+	}),
+	endpoints: () => ({}),
+});
+
+export default baseApi;
