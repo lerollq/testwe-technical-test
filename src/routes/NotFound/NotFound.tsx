4e084fa41b37e7c2226e13bcbeea2d9feaa642df
Quentin Le Rolland <qlerolland@gmail.com>
2022-12-10 14:22:19 +0100

    feat(routes): add 404 Not Found page

--- src/routes/NotFound/NotFound.tsx
+++ src/routes/NotFound/NotFound.tsx
@@ -1,5 +1,19 @@
+import { Button } from "@/components/atoms";
+import { useNavigate } from "react-router-dom";
+
 function NotFound() {
-	return <div>404</div>;
+	const navigate = useNavigate();
+
+	const goBack = () => {
+		return navigate(-1);
+	};
+
+	return (
+		<div>
+			<Button onClick={goBack}>Go Back</Button>
+			404
+		</div>
+	);
 }
 
 export default NotFound;
